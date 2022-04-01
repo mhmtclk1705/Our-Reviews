7f5da7bcd8656bd1333dd3cb50eda7bfc41f611b
mhmtclk1705 <mehmetcelik1705@gmail.com>
2022-04-02 01:10:54 +0300

    comment

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Review from './Review';
 function App() {
   return(
+    // appjs içerisinde pek bişey yapmadık
+    // hazır css üzerine html tagleri oluşturduk
   <main>
     <section className="container">
       <div className="title">
+        {/* capitalize */}
         <h2>our reviews</h2>
         <div className="underline"></div>
       </div>
